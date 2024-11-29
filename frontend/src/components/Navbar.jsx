60c6dbe18eab3a8c578fb552a4d5eccce9099d27
Malouzi <marie.zirzlaff@dci-student.org>
2024-11-29 11:31:10 +0100

    React icon bar eingefügt

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { HiMiniBars3CenterLeft } from "react-icons/hi2";
 
 const Navbar = () => {
   return (
     <header className='max-w-screen-2xl mx-auto px-4 py-6'>
-        <nav>
+        <nav className='flex justify-between items-center'>
             {/* left side */}
-            <div>Logo & search bar</div>
+            <div>
+                <Link to="/">
+                <HiMiniBars3CenterLeft />
+                </Link>
+            </div>
 
             {/* right side */}
             <div>
