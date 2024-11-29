db46a2b736d5c7e534546358aa2ad73f0952db90
Malouzi <marie.zirzlaff@dci-student.org>
2024-11-29 11:17:10 +0100

    Navbar anlegen

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='max-w-screen-2xl mx-auto px-4 py-6'>
+        <nav>
+            {/* left side */}
+            <div>Logo & search bar</div>
+
+            {/* right side */}
+            <div>
+                nav items
+            </div>
+        </nav>
+
+    </header>
+  )
+}
+
+export default Navbar;
