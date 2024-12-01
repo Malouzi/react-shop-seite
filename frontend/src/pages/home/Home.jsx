bb314be13ccbd7e78c141044320d5f54a4b36a51
Malouzi <marie.zirzlaff@dci-student.org>
2024-12-01 22:19:32 +0100

    Recommended Section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSellers from './TopSellers'
+import Recommended from './Recommended'
+
 
 const Home = () => {
   return (
     <>
         <Banner/>
         <TopSellers/>
+        <Recommended/>
     </>
   )
 }
