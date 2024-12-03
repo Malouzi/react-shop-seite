7f6b452c18a94dc8440c025403e1d43832cb712c
Malouzi <marie.zirzlaff@dci-student.org>
2024-12-03 13:34:15 +0100

    Login und Regestration

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='h-[calc(100vh-120px)] flex justify-center items-center'>
+        <div className='w-full max-w-sm'>
+            <h2 className='text-xl font-semibold mb-4'>Login</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Login
