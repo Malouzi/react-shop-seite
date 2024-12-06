bfc82c93553f58a4b23733a26d772d89c67c8a5d
Malouzi <marie.zirzlaff@dci-student.org>
2024-12-06 13:28:31 +0100

    addToCartHandle eingefügt

--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -1,1 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    cartItems: []
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: initialState,
+    reducers:{
+        addToCart: (state, action) => {
+            const existingItem = state.cartItems.find(item => item._id === action.payload._id);
+            if(!existingItem) {
+                state.cartItems.push(action.payload)
+                alert("Item added successfully")
+                } else(
+                    allert("Item already exists")
+                )
+            }
+        }
+    })
+
+    // export the actions
+    export const  {addToCart, removeFromCart, clearCart} = cartSlice.actions;
+    export default cartSlice.reducer;
