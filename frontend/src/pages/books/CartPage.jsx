74f2bf644e16eb6a4e0cbc5a7072c9a770c34ade
Malouzi <marie.zirzlaff@dci-student.org>
2024-12-12 09:50:37 +0100

    CartPage

--- frontend/src/pages/books/CartPage.jsx
+++ frontend/src/pages/books/CartPage.jsx
@@ -24,14 +24,14 @@ const CartPage = () => {
     <div className="flex mt-12 h-full flex-col overflow-hidden bg-white shadow-xl">
     <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
       <div className="flex items-start justify-between">
-        <div className="text-lg font-medium text-gray-900">Shopping cart</div>
+        <div className="text-lg font-medium text-gray-900">Warenkorb</div>
         <div className="ml-3 flex h-7 items-center ">
           <button
             type="button"
             onClick={handleClearCart }
             className="relative -m-2 py-1 px-2 bg-red-500 text-white rounded-md hover:bg-secondary transition-all duration-200  "
           >
-            <span className="">Clear Cart</span>
+            <span className="">Warenkorb löschen</span>
           </button>
         </div>
       </div>
@@ -61,16 +61,16 @@ const CartPage = () => {
                                                                 </h3>
                                                                 <p className="sm:ml-4">€{product?.newPrice}</p>
                                                             </div>
-                                                            <p className="mt-1 text-sm text-gray-500 capitalize"><strong>Category: </strong>{product?.category}</p>
+                                                            <p className="mt-1 text-sm text-gray-500 capitalize"><strong>Kategorie: </strong>{product?.category}</p>
                                                         </div>
                                                         <div className="flex flex-1 flex-wrap items-end justify-between space-y-2 text-sm">
-                                                            <p className="text-gray-500"><strong>Qty:</strong> 1</p>
+                                                            <p className="text-gray-500"><strong>Menge</strong> 1</p>
 
                                                             <div className="flex">
                                                                 <button
                                                                 onClick={() => handleRemoveFromCart(product)}
                                                                 type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                                                    Remove
+                                                                    Entfernen
                                                                 </button>
                                                             </div>
                                                         </div>
@@ -89,27 +89,27 @@ const CartPage = () => {
 
     <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
       <div className="flex justify-between text-base font-medium text-gray-900">
-        <p>Subtotal</p>
+        <p>Gesamt</p>
         <p>€{totalPrice ? totalPrice : 0}</p>
       </div>
-      <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+      <p className="mt-0.5 text-sm text-gray-500">zzgl. Versandkosten</p>
       <div className="mt-6">
         <Link
           to="/checkout"
           className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
         >
-          Checkout
+          Zur Kasse
         </Link>
       </div>
       <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
         <Link to="/">
-          or 
+          oder 
           <button
             type="button"
 
             className="font-medium text-indigo-600 hover:text-indigo-500 ml-1"
           >
-            Continue Shopping
+            Einkauf fortsetzen
             <span aria-hidden="true"> &rarr;</span>
           </button>
         </Link>
